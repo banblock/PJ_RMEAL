212e10b07b6067baaaed124fbeb084d3fe541853
banblock <99329720+banblock@users.noreply.github.com>
2024-07-20 21:11:37 +0900

    add prompt & AI dir

--- lib/src/ai/geminiAPI.dart
+++ lib/src/ai/geminiAPI.dart
@@ -1,16 +1,15 @@
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-class geminiAI {
 
-  void RecommendRecipeModel()async {
-    await dotenv.load();
-    final key = dotenv.get("GEMINI_API_KEY");
+class GeminiAI {
+
+  void RecommendRecipeModel(final key)async {
     final model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: key);
   }
 
-  void getRecipePrompt(){
-
+  void SummarizeRecipeModel(final key) async{
+    final model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: key);
   }
 
-  
+
 }
