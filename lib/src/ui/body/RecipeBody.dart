48c538a4fa3090c2104a8457f4e464736092dd3c
banblock <99329720+banblock@users.noreply.github.com>
2024-07-31 19:24:26 +0900

    add recipebody

---
+++ lib/src/ui/body/RecipeBody.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class Recipebody extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+        padding: EdgeInsets.all(8)
+    );
+  }
+
+}
