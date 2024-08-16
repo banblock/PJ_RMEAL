998f5e7b13fa1854c2d55d853ed7db33161fce3d
banblock <99329720+banblock@users.noreply.github.com>
2024-08-16 17:44:29 +0900

    fix debuging code

--- lib/src/ai/geminiAPI.dart
+++ lib/src/ai/geminiAPI.dart
@@ -63,7 +63,7 @@ class GeminiAI {
     String text = chatprompt!.inputPrompt();
     final content = [Content.text(text)];
     final response = await model.generateContent(content);
-    print(response.text);
+    print('output respone');
     setResponse(response.text);
   }
 
